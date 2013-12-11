0d035ab35b7323426e1a92a6326856ba3a203276
Rainrider <rainrider.wow@gmail.com>
2013-12-11 23:24:17 +0100

    switch to the events introduced in patch 5.4.2 and ditch the dependancy on the encounter journal
    Fixes #1

--- rainBossWhisperer.lua
+++ rainBossWhisperer.lua
@@ -1,34 +1,26 @@
 local addon = ...
 local prefix = "<RBW>: "
 local dndMsg = prefix .. "Encounter in progress %s: %s"
-local combatEndedMsg = prefix .. "Combat ended."
+local combatEndedWin = prefix .. "Combat ended. Win against %s."
+local combatEndedWipe = prefix .. "Combar ended. Wipe against %s."
 local bossFormat = " %s (%d%%)" -- name (health%)
 
 local playerName = UnitName("player")
 local disableChatFilter = true
 
-local bosses = {}
 local whisperers = {}
-local encounters = {}
---[[
-	[encounterIndex] = {
-		[link] = select(5, GetEncounterInfoByIndex(encounterIndex, instanceID))
-		[bossName1] = true
-		[bossName2] = true
-	}
-]]
-local currentEncounterLink = ""
 
-local debug = true
+local encounterLinkFormat = "|cff66bbff|Hjournal:1:%d:%d|h[%s]|h|r" -- encounterID, difficultyID, name
+local encounterLink
+
+local debug = false
 
 local frame = CreateFrame("Frame")
-frame:SetScript("OnEvent", function(self, event, msg, ...) self[event](self, msg, ...) end)
-frame:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
-frame:RegisterEvent("PLAYER_REGEN_ENABLED")
-frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:SetScript("OnEvent", function(self, event, ...) self[event](self, ...) end)
+frame:RegisterEvent("ENCOUNTER_START")
+frame:RegisterEvent("ENCOUNTER_END")
 frame:RegisterEvent("CHAT_MSG_WHISPER")
 frame:RegisterEvent("CHAT_MSG_BN_WHISPER")
-frame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 frame:RegisterEvent("ADDON_LOADED")
 
 local function Debug(...)
@@ -49,104 +41,38 @@ local function GetReply(sender, msg, presenceID, client)
 				str = str .. string.format(bossFormat, UnitName(unit), math.floor(UnitHealth(unit) / UnitHealthMax(unit) * 100 + 0.5))
 			end
 		end
-		-- TODO: message length should not be > 255 characters (utf8 aware)
-		--		 SendChatMessage truncates to 255 chars, BNSendWhisper fails silently
-		--		 use strlenutf8()
-		return string.format(dndMsg, currentEncounterLink, str)
+		-- message length should not be > 255 characters (utf8 aware)
+		-- SendChatMessage truncates to 255 chars, BNSendWhisper fails silently
+		local reply = string.format(dndMsg, encounterLink, str)
+
+		if strlenutf8(reply) > 255 then
+			reply = string.format(dndMsg, encounterLink, "")
+		end
+
+		return reply
 	end
 end
 
-function frame:PLAYER_ENTERING_WORLD()
-	self:ZONE_CHANGED_NEW_AREA()
-	self:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
+function frame:ENCOUNTER_START(encounterID, name, difficultyID, size)
+	encounterLink = string.format(encounterLinkFormat, encounterID, difficultyID, name)
 end
 
--- XXX: does not fire after reloadui
-function frame:ZONE_CHANGED_NEW_AREA()
-	if not IsAddOnLoaded("Blizzard_EncounterJournal") then
-		LoadAddOn("Blizzard_EncounterJournal")
-	end
-
-	local instanceID = EJ_GetCurrentInstance()
-	
-	if not instanceID or instanceID == 0 or instanceID == 322 then
-		Debug("Not in an instance", instanceID)
-		return
-	end
-
-	EJ_SelectInstance(instanceID) -- load instance data
-
-	local encounterIndex = 1
-	local _, _, encounterID, _, encounterLink = EJ_GetEncounterInfoByIndex(encounterIndex, instanceID)
-	while encounterID do
-		encounters[encounterIndex] = {}
-		encounters[encounterIndex]["link"] = encounterLink
-
-		local creatureIndex = 1
-		local _, bossName = EJ_GetCreatureInfo(creatureIndex, encounterID)
-		while bossName do
-			encounters[encounterIndex][bossName] = true
-			creatureIndex = creatureIndex + 1
-			_, bossName = EJ_GetCreatureInfo(creatureIndex, encounterID)
-		end
-
-		encounterIndex = encounterIndex + 1
-		_, _, encounterID, _, encounterLink = EJ_GetEncounterInfoByIndex(encounterIndex, instanceID)
-	end
-
-	if debug then
-		if not IsAddOnLoaded("Blizzard_DebugTools") then
-			LoadAddOn("Blizzard_DebugTools")
-		end
-		DevTools_Dump(encounters)
-	end
-
-end
-
--- XXX: does not fire after reloadui
-function frame:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
-	if currentEncounterLink ~= "" then return end -- so we don't set this multiple times per encounter
-
-	wipe(bosses)
-	for i = 1, MAX_BOSS_FRAMES do
-		local unit = "boss" .. i
-		-- Shado-Pan Garrison daily quests display your companion as a boss
-		-- UnitClassification returns "normal" and UnitName returns nil for non-present units
-		if UnitClassification(unit) ~= "normal" then
-			bosses[#bosses + 1] = UnitName(unit)
-		end
-	end
-
-	Debug("Bosses:", #bosses, "CurrentEncounter:", currentEncounterLink)
-
-	for i = 1, #bosses do
-		for j = 1, #encounters do
-			if encounters[j][bosses[i]] then
-				currentEncounterLink = encounters[j]["link"]
-				break
-			end
-		end
-		if currentEncounterLink ~= "" then break end
-	end
-end
-
--- TODO: does this fire for dead units?
-function frame:PLAYER_REGEN_ENABLED()
+function frame:ENCOUNTER_END(_, _, _, _, success)
+	local reply = string.format(success == 1 and combatEndedWin or combatEndedWipe, encounterLink)
 	for player in pairs(whisperers) do
 		local presenceID = tonumber(player)
 		if presenceID then
-			BNSendWhisper(presenceID, combatEndedMsg)
+			BNSendWhisper(presenceID, reply)
 		else
-			SendChatMessage(combatEndedMsg, "WHISPER", nil, player)
+			SendChatMessage(reply, "WHISPER", nil, player)
 		end
 	end
-	currentEncounterLink = ""
-	wipe(bosses)
+	encounterLink = nil
 	wipe(whisperers)
 end
 
 function frame:CHAT_MSG_WHISPER(msg, sender, _, _, _, flag)
-	if flag == "GM" or (#bosses == 0 and currentEncounterLink == "") then return end
+	if flag == "GM" or not encounterLink then return end
 
 	local reply = GetReply(sender, msg)
 	if reply then
@@ -154,10 +80,10 @@ function frame:CHAT_MSG_WHISPER(msg, sender, _, _, _, flag)
 	end
 end
 
-function frame:CHAT_MSG_BN_WHISPER(msg, sender, _, _, _, _, _, _, _, _, _, _, presenceID)
-	if #bosses == 0 and currentEncounterLink == "" then return end
+function frame:CHAT_MSG_BN_WHISPER(msg, _, _, _, _, _, _, _, _, _, _, _, presenceID)
+	if not encounterLink then return end
 
-	local _, _, _, _, toonName, _, client = BNGetFriendInfoByID(presenceID) -- client: WoW, D3
+	local _, _, _, _, toonName, _, client = BNGetFriendInfoByID(presenceID) -- client: WoW, D3, ...
 	local reply = GetReply(toonName, msg, presenceID, client)
 	if reply then
 		BNSendWhisper(presenceID, reply)
